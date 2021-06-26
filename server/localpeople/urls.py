b59e44fced1024a716ff6069445d1384e349a505
hyoilkim <quf9483@naver.com>
2021-06-26 19:21:42 +0900

    Modify django settings

--- server/localpeople/urls.py
+++ server/localpeople/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import urls
-from posting import urls
+import rest_framework
+import posting
 from django.conf import settings
 from django.conf.urls.static import static
 
