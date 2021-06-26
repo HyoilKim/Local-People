df7d1bf5214e8699ddef9219951c29afa5ad903f
epikjjh <johyung@molocoads.com>
2021-06-26 15:03:11 +0900

    Convert django settings

--- server/localpeople/urls.py
+++ server/localpeople/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import urls
-from posting import urls
 from django.conf import settings
 from django.conf.urls.static import static
 
