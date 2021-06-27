201fb08c97074f7609f323f80003dbd61c2bb3b4
justin1228 <didls1228@hanyang.ac.kr>
2021-06-27 23:02:18 +0900

    feat: KakaoMap with current location marker

---
+++ frontend/src/components/map/Map.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect } from "react";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const Map = () => {
+  useEffect(() => {
+    let container = document.getElementById("map");
+    let options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    let map = new window.kakao.maps.Map(container, options);
+
+    function displayMarker(locPosition, message) {
+      // 마커를 생성합니다
+      var marker = new window.kakao.maps.Marker({
+        map: map,
+        position: locPosition,
+      });
+
+      var iwContent = message, // 인포윈도우에 표시할 내용
+        iwRemoveable = true;
+
+      // 인포윈도우를 생성합니다
+      var infowindow = new window.kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: iwRemoveable,
+      });
+
+      // 인포윈도우를 마커위에 표시합니다
+      infowindow.open(map, marker);
+
+      // 지도 중심좌표를 접속위치로 변경합니다
+      map.setCenter(locPosition);
+    }
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude, // 위도
+          lon = position.coords.longitude; // 경도
+
+        var locPosition = new window.kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+          message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+
+        // 마커와 인포윈도우를 표시합니다
+        displayMarker(locPosition, message);
+      });
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+      var locPosition = new window.kakao.maps.LatLng(33.450701, 126.570667),
+        message = "geolocation을 사용할수 없어요..";
+
+      displayMarker(locPosition, message);
+    }
+    
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="map" style={{ width: "400px", height: "400px" }} />
+    </div>
+  );
+};
+
+export default Map;
