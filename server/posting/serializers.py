d4e6f141135a1fc936366699b942189b2151bafb
hyoilkim <quf9483@naver.com>
2021-06-29 11:30:57 +0900

    fix: Allow blank image when posting

--- server/posting/serializers.py
+++ server/posting/serializers.py
@@ -2,7 +2,7 @@ from .models import Posting, Comment
 from rest_framework import serializers
 
 class PostingSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
+    image = serializers.ImageField(use_url=True, required=False, allow_null=True) # 둘 중 하나만 넣어도 됨
     user_id = serializers.ReadOnlyField(source='user.username')
     class Meta:
         model = Posting
