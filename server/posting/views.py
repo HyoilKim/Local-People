581f6ced66b0592efd87fa359fb4a7952c2ecc0c
Hyoil Kim <quf9483@naver.com>
2021-06-29 20:35:36 +0900

    fix: Remove local authentication classes

--- server/posting/views.py
+++ server/posting/views.py
@@ -1,25 +1,24 @@
+import json
+from django.http import JsonResponse
+from django.core.checks import messages
 from .models import Posting, Comment
 from .serializers import PostingSerializer, CommentSerializer
 from rest_framework import viewsets, mixins
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly
 
 class PostingViewSet(viewsets.ReadOnlyModelViewSet):
-    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
-
     queryset = Posting.objects.all()
     serializer_class = PostingSerializer
-
     filter_backends = [SearchFilter]
     search_fields = ('content', 'category') # only tuple
 
 class MyPostingViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
-    
     queryset = Posting.objects.all()
     serializer_class = PostingSerializer
     
@@ -28,13 +27,11 @@ class MyPostingViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         qs = Posting.objects.all()
-        qs = qs.filter(user=self.request.user)           
+        qs = qs.filter(user=self.request.user)   
         return qs
 
 class CommentViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication, SessionAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
