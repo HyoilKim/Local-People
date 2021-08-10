402a6e274f1e13c1448cc7c69072d6ce73bcbf98
justin1228 <didls1228@hanyang.ac.kr>
2021-08-10 20:02:12 +0900

    fetching error

--- frontend/src/components/mainpage/MainPage.js
+++ frontend/src/components/mainpage/MainPage.js
@@ -8,57 +8,90 @@ import Nav from "../nav/Nav";
 const MainPage = () => {
   const [feeds, setFeeds] = useState([]);
   const [isMap, setIsMap] = useState("false");
-  const handleClick = () => {
-    setIsMap(!isMap);
+  const [currentLocation, setCurrentLocation] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  function getDistanceFromLatLonInKm(lat1, lng1, lat2, lng2) {
+    //두 점의 위경도좌표를 받아 거리 return
+    function deg2rad(deg) {
+      return deg * (Math.PI / 180);
+    }
+    const R = 6371;
+    const dLat = deg2rad(lat2 - lat1);
+    const dLon = deg2rad(lng2 - lng1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) *
+        Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
+    
+    return d;
+  }
+
+  async function getPosition(options){
+    return new Promise(function (resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      
+    });
   };
+
+  async function getFeeds(options){
+    return new Promise((resolve, reject)=>{
+      
+      db.collection("feeds")
+    .orderBy("timestamp", "desc")
+    .onSnapshot((snapshot) => {
+      //every single time a new feeds is added, this code runs
+        
+          resolve(snapshot.docs.map((doc) => ({ id: doc.id, feed: doc.data() })));
+
+      })
+      
+    }
+    )}
+
+  const fetchFeeds = async () => {
+    const f = await getFeeds();
+    setFeeds(f);
+  }
+
+  const fetchPosition = async () => {
+    const p = await getPosition(); //geolocation에서 현재 위치를 받아온다.
+    const pObject = {latitude: p.coords.latitude, longitude: p.coords.longitude};
+    setCurrentLocation(pObject); //현재위치의 좌표를 currentLocation에 넣는다.
+    
+  }
+
+  async function filterFeeds() {
+    console.log(currentLocation);
+    console.log(feeds);
+    setFeeds(feeds => {feeds.filter(({id, feed}) => getDistanceFromLatLonInKm(
+      currentLocation.latitude,
+      currentLocation.longitude,
+      feed.location.lat,
+      feed.location.lon
+    ) < 10)})
+  }
+
 
   useEffect(() => {
     //this is where the code runs
-    function getDistanceFromLatLonInKm(lat1, lng1, lat2, lng2) {
-      //두 점의 위경도좌표를 받아 거리 return
-      function deg2rad(deg) {
-        return deg * (Math.PI / 180);
-      }
-      const R = 6371;
-      const dLat = deg2rad(lat2 - lat1);
-      const dLon = deg2rad(lng2 - lng1);
-      const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(deg2rad(lat1)) *
-          Math.cos(deg2rad(lat2)) *
-          Math.sin(dLon / 2) *
-          Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      const d = R * c;
-      
-      return d;
-    }
-    let getPosition =(options) => {
-      return new Promise(function (resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject, options);
-      });
-    };
 
-    db.collection("feeds")
-      .orderBy("timestamp", "desc")
-      .onSnapshot((snapshot) => {
-        //every single time a new feeds is added, this code runs
-        getPosition()
-        .then((position) => {
-          setFeeds(
-            snapshot.docs.map((doc) => ({ id: doc.id, feed: doc.data() })).filter(({id, feed}) => getDistanceFromLatLonInKm(
-              position.coords.latitude,
-              position.coords.longitude,
-              feed.location.lat,
-              feed.location.lon
-            ) < 10)
-            
-          );
-        })
-        .catch((e) => {
-          console.log(e.message);
-        })
-      });
+  async function loadFeeds() {
+    console.time();
+    const promises = [];
+    promises.push(fetchFeeds());
+    promises.push(fetchPosition());
+    await Promise.all(promises);
+    console.log("load complete \n");
+    console.timeEnd();
+
+    return new Promise((resolve) => {resolve();})
+  }
+    loadFeeds().then(filterFeeds());
 
   }, []);
 
@@ -67,8 +100,8 @@ const MainPage = () => {
       <Nav />
       <div className="app__feed">
         <div className="app__body">
-          {isMap === true ? (
-            <MarkerView feeds={feeds}></MarkerView>
+          {feeds == 0 ? (
+            <div>로딩중입니다.</div>
           ) : (
             feeds.map(({ id, feed }) => (
               <Feed
@@ -86,7 +119,8 @@ const MainPage = () => {
         </div>
         <div className="app__map__container">
           <div className="app__map">
-            <MarkerView feeds={feeds}></MarkerView>
+            {feeds == 0 ? (<div></div>): (<MarkerView feeds={feeds}></MarkerView>)}
+            
           </div>
         </div>
       </div>
